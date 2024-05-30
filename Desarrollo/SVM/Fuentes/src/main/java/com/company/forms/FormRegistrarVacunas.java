53891877ac6d51a44a00866a8c11a26cce312eed
Pablo Mendoza <126980655+yuifo@users.noreply.github.com>
2024-05-29 22:30:55 -0500

    Update FormRegistrarVacunas.java

--- Desarrollo/SVM/Fuentes/src/main/java/com/company/forms/FormRegistrarVacunas.java
+++ Desarrollo/SVM/Fuentes/src/main/java/com/company/forms/FormRegistrarVacunas.java
@@ -1,1 +1,8 @@
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
